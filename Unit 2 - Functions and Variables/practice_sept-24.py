e53e00fb7b512484e1d90ab78d2e612d3a4a6b77
keatonwilaby27 <820762@mystma.org>
2024-09-25 09:13:58 -0500

    animal survey turn in

---
+++ Unit 2 - Functions and Variables/practice_sept-24.py
@@ -0,0 +1,6 @@
+entry = input("Enter a number\n")   #asking for input and declaring a varible
+entry = int(entry)      #converting input to an integer, assignment
+print(entry*10)     #
+word_one = input("enter first word!\n")
+word_two = input("enter the second word\n")
+print(word_one+word_two)
---
+++ assignments/animalsurvey.py
@@ -0,0 +1,5 @@
+Q1= input("What is your favorite animal?\n")
+Q2= input("Why do you like them?\n")
+Q3= input("Where do they live?\n")
+Q4= input("What is one fun fact about them?\n")
+print ("your favorite animal is the"+" "+ Q1 +" "+"you like them because"+" "+ Q2 +" "+"they live in"+" "+ Q3 +" "+ "1 fun fact is"+" " + Q4 )
