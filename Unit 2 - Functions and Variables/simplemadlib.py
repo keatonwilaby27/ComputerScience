b79745f85be9493213c762ff97bc5b72bc2b7caf
keatonwilaby27 <820762@mystma.org>
2024-09-26 09:31:50 -0500

    madlab

---
+++ Unit 2 - Functions and Variables/simplemadlib.py
@@ -0,0 +1,18 @@
+# Once upon a time, in a old castle 
+# There lived a large dragon.
+#Every day, the dragon would eat people
+# One day, the dragon escaped the castle
+
+#ask for a place
+#ask for a charactor
+#ask what it did every day
+#ask what it would do
+#ask what it was
+#ask what it escaped
+line1= input("Once upon a time, in a \n")
+line2= input("There lived a\n")
+line3= input("Everyday the\n")
+line4= input("Would,\n")
+line5= input("One day the\n")
+line6= input("escaped the")
+print ("Once upon a time, in a "+ line1 + " There lived a " +  line2 +" every day the "+ line3 +" would "+line4+" one day the "+line5+" escaped the "+line6)
