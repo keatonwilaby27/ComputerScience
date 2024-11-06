2229fb701ec440ac8decd03c1cd437ac146cbf87
keatonwilaby27 <820762@mystma.org>
2024-11-06 09:45:31 -0600

    quick sort

--- assignments/manualsort.py
+++ assignments/manualsort.py
@@ -1,10 +1,23 @@
 def bubble_sort(list):
+    steps = 0
     for n in range(len(list)-1, 0, -1):
         for i in range(n):
             if list[i] > list[i+1]:
                 list[i], list[i+1] = list[i+1], list[i]
-            
-list = [5,3,4,7,1,2,6]
+                steps +=1
+    print(list)
+    print("completed in " + str(steps)+ " steps")   
+list = []
 
 bubble_sort(list)
 print(list)
+
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        pivot = arr[0]
+        less_than_pivot = [x for x in arr[1:] if x <= pivot]
+        greater_than_pivot = [x for x in arr[1:] if x > pivot]
+        return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
